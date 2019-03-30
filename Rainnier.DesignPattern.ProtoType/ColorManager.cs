b34e1e2c3557f582c5846fac456809bd287de628
Richard Chen <v-juche@microsoft.com>
2019-03-30 23:08:59 +0800

    update prototype

---
+++ Rainnier.DesignPattern.ProtoType/ColorManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.DesignPattern.ProtoType
+{
+    public class ColorManager
+    {
+        private Dictionary<string, Color> dic => new Dictionary<string, Color>();
+
+        public Color this[string colorName]
+        {
+            get
+            {
+                return dic[colorName];
+            }
+            set
+            {
+                dic.Add(colorName, value);
+            }
+        }
+    }
+}
---
+++ Rainnier.DesignPattern.ProtoType/ColorProtoType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.DesignPattern.ProtoType
+{
+    public abstract class ColorProtoType : ICloneable
+    {
+        public abstract object Clone();
+    }
+}
