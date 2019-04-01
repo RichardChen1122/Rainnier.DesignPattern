39183ed1be1ae2d5bb695de84c98054112cefe8a
Richard Chen <v-juche@microsoft.com>
2019-04-01 09:29:27 +0000

    Update

--- Rainnier.DesignPattern.Factory/Program.cs
+++ Rainnier.DesignPattern.Factory/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rainnier.DesignPattern.Factory.AbstractFactory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace Rainnier.DesignPattern.Factory
     {
         static void Main(string[] args)
         {
+            var factory = new SpringSkinFactory();
+
+            var button =factory.CreateButton();
+            button.Display();
+            Console.ReadKey();
         }
     }
 }
