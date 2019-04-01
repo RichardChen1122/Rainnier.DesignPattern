f31e06023e13e6f7ed2d0f55368bdda9a3d2f464
Richard Chen <v-juche@microsoft.com>
2019-04-01 07:17:31 +0000

    Update Builder

--- Rainnier.DesignPattern.Builder/Program.cs
+++ Rainnier.DesignPattern.Builder/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rainnier.DesignPattern.Builder.Structural;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,17 @@ namespace Rainnier.DesignPattern.Builder
     {
         static void Main(string[] args)
         {
+            Director director = new Director();
+
+            Rainnier.DesignPattern.Builder.Structural.Builder b1 = new ConcreteBuilder1();
+            Rainnier.DesignPattern.Builder.Structural.Builder b2 = new ConcreteBuilder2();
+
+            // Construct two products
+            Product p1 = director.Construct(b1);
+            p1.Show();
+
+            Product p2 = director.Construct(b2);
+            p2.Show();
         }
     }
 }
---
+++ Rainnier.DesignPattern.Builder/Structural/Director.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.DesignPattern.Builder.Structural
+{
+    public class Director
+    {
+        public Product Construct(Builder builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+
+            return builder.GetResult();
+        }
+    }
+}
