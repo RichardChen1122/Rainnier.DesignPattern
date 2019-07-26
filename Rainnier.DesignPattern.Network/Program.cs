fa621c9aff7c9c46f9092c14ec7144e39b133eb2
Tu Zhanle <tzhanl@hotmail.com>
2019-07-26 01:39:57 -0700

    Update

---
+++ Rainnier.DesignPattern.Network/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Net.Sockets;
+
+namespace Rainnier.DesignPattern.Network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 8080));
+            serverSocket.Listen(128);
+            serverSocket.BeginAccept(null, 0, OnAccept, null);
+
+            void OnAccept(IAsyncResult result)
+            {
+                try
+                {
+                    Socket client = null;
+                    if (serverSocket != null && serverSocket.IsBound)
+                    {
+                        client = serverSocket.EndAccept(result);
+                    }
+                    if (client != null)
+                    {
+                        /* Handshaking and managing ClientSocket */
+                    }
+                }
+                catch (SocketException exception)
+                {
+
+                }
+                finally
+                {
+                    if (serverSocket != null && serverSocket.IsBound)
+                    {
+                        serverSocket.BeginAccept(null, 0, OnAccept, null);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Rainnier.DesignPattern.Thread/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rainnier.DesignPattern.ThreadSync
+{
+    class Program
+    {
+        static volatile int ticketCount = 10;
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(SellTicket)
+            {
+                Name = "Windows1"
+            };
+            var t2 = new Thread(SellTicket);
+            t2.Name = "Windows2";
+            var t3 = new Thread(SellTicket);
+            t3.Name = "Windows3";
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Console.ReadKey();
+
+        }
+
+        static void SellTicket()
+        {
+            //while (Interlocked.CompareExchange(ref ticketCount,0,0)>0)
+            while (ticketCount > 0)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} currently have {ticketCount} ticket");
+                Interlocked.Decrement(ref ticketCount);
+                //ticketCount--;
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} sold 1 ticket. left {ticketCount}");
+            }
+        }
+    }
+}
