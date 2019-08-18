2763df405e00189f222f47175d0e726b5c56cd6c
Tu Zhanle <tzhanl@hotmail.com>
2019-08-18 23:52:03 +0800

    update

--- Rainnier.DesignPattern.Asynchronous/Program.cs
+++ Rainnier.DesignPattern.Asynchronous/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Rainnier.DesignPattern.Asynchronous
@@ -10,7 +11,54 @@ namespace Rainnier.DesignPattern.Asynchronous
     {
         static void Main(string[] args)
         {
-            // do some change
+            //List<TestEntity> test = new List<TestEntity>();
+            //List<Task> tasks = new List<Task>();
+            //test.Add(new TestEntity() { dbName = "db1", time = 11 });
+            //test.Add(new TestEntity() { dbName = "db2", time = 10 });
+            //test.Add(new TestEntity() { dbName = "db3", time = 9 });
+            //test.Add(new TestEntity() { dbName = "db4", time = 8 });
+            //test.Add(new TestEntity() { dbName = "db5", time = 7 });
+            //test.Add(new TestEntity() { dbName = "db6", time = 6 });
+            //test.Add(new TestEntity() { dbName = "db7", time = 5 });
+            //test.Add(new TestEntity() { dbName = "db8", time = 4 });
+            //test.Add(new TestEntity() { dbName = "db9", time = 3 });
+            //test.Add(new TestEntity() { dbName = "db10", time = 2 });
+            //foreach(var a in test)
+            //{
+            //    Task.Run(() => TestClass.DoInsertInDatabase(a.dbName, a.time)).
+            //        ContinueWith(m => { Console.WriteLine($"{a.dbName} done"); });
+            //}
+
+            using (ManualResetEventSlim finishEvent = new ManualResetEventSlim(true))
+            {
+                List<int> tempResult = null;
+                var thread = new Thread(() =>
+                {
+                    tempResult = TestClass.DoSeachInDatabase();
+                    finishEvent.Set();
+                });
+                thread.Start();
+                
+                int t = 1;
+                while (t > 0)
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("doing something else");
+                    t--;
+                }
+                finishEvent.Wait();
+                Console.WriteLine(tempResult.Count);
+            }
+
+            Console.ReadKey();
+
+
         }
+    }
+
+    public class TestEntity
+    {
+        public int time { get; set; }
+        public string dbName { get; set; }
     }
 }
---
+++ Rainnier.DesignPattern.Asynchronous/TestClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rainnier.DesignPattern.Asynchronous
+{
+    public class TestClass
+    {
+        public static List<int> DoSeachInDatabase()
+        {
+            Thread.Sleep(5000);
+            return Enumerable.Range(0, 100).ToList();
+        }
+
+        public static void DoInsertInDatabase(string db, int time)
+        {
+            int i = 0;
+            while (i < time*1000)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine($"Doing insert action in databse {db} in Thread {Thread.CurrentThread.ManagedThreadId}");
+                
+                i += 500;
+
+            }
+
+        }
+
+    }
+
+    
+}
