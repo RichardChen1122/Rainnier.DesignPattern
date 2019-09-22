a7bc064b3a68b63d03809cea6015351d8902438f
Tu Zhanle <tzhanl@hotmail.com>
2019-09-22 21:53:57 +0800

    update

---
+++ Rainnier.DesignPattern.Rx.Demo/BomberControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.DesignPattern.Rx.Demo
+{
+    public class BomberControl : IDisposable
+    {
+        private IDisposable planeSpottedSubscription;
+
+        public BomberControl(JetFighter jetFighter)
+        {
+            this.planeSpottedSubscription = jetFighter.PlaneSpotted.Subscribe(this.OnPlaneSpotted);
+        }
+
+        public void Dispose()
+        {
+            this.planeSpottedSubscription.Dispose();
+        }
+
+        private void OnPlaneSpotted(JetFighter jetFighter)
+        {
+            JetFighter spottedPlane = jetFighter;
+        }
+    }
+}
