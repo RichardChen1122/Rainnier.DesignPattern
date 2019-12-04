4b341df4ea201b517891741c81138e2570d30ea8
Tu Zhanle <tzhanl@hotmail.com>
2019-12-04 11:16:20 +0800

    update

---
+++ Rainnier.DesignPattern.Thread/LazyDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainnier.DesignPattern.ThreadSync
+{
+    public class LazyDemo
+    {
+        static void Main(string[] args)
+        {
+            Lazy<DateTime> time = new Lazy<DateTime>(() => DateTime.Now);
+
+            Console.WriteLine(time.IsValueCreated);
+            Console.WriteLine(time.Value);
+            Console.WriteLine(time.IsValueCreated);
+            Console.WriteLine(time.Value);
+
+            Console.ReadKey();
+        }
+    }
+}
