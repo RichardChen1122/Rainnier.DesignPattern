befe473a388b31ed82ff208841893c9a4583a0df
RichardChen1122 <rchen89@126.com>
2021-12-27 15:26:59 +0800

    Add new code snippet

---
+++ Rainnier.DesignPattern.Thread/Concurrent/OnceExecutionSimaluatoins.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rainnier.DesignPattern.Thread.Concurrent
+{
+    internal class OnceExecutionSimaluatoins
+    {
+        private  static int _networkOperationsInProgress;
+        public async Task RefreshAsync(CancellationToken cancellationToken)
+        {
+            // Ensure that concurrent threads do not simultaneously execute refresh operation. 
+            if (Interlocked.Exchange(ref _networkOperationsInProgress, 1) == 0)
+            {
+                try
+                {
+                    //Anything need do
+                }
+                finally
+                {
+                    Interlocked.Exchange(ref _networkOperationsInProgress, 0);
+                }
+            }
+        }
+    }
+}
